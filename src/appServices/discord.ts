567ccff71db8c4e116f95bcabc742058cc03a13b
Paulo Abreu <paulo.santana@luby.software>
2022-09-25 02:35:35 -0300

    refactor: 🏗️ change extension of files based on environment running

--- src/appServices/discord.ts
+++ src/appServices/discord.ts
@@ -1,4 +1,7 @@
 import { prefix } from "../shared/services/config/discord";
+import { envHelper } from "../shared/utils/envHelper";
+
+const ENV = envHelper.env;
 
 export function botStarted() {
   console.log("Discord bot is ready");
@@ -10,8 +13,10 @@ export async function sendMessage(message: any) {
   const args = message.content.trim().slice(prefix.length).split(/ +/g);
   const command = args.shift().toLowerCase();
 
+  const extension = ENV === "prod" ? ".js" : ".ts";
+
   try {
-    const commandFile = require(`../commands/${command}.ts`);
+    const commandFile = require(`../commands/${command}${extension}`);
     commandFile.execute({ args, message });
   } catch (error) {
     console.log(error);
--- src/shared/utils/helpMessage.ts
+++ src/shared/utils/helpMessage.ts
@@ -5,7 +5,11 @@ const commands = fs.readdirSync(path.resolve(__dirname, "../..", "commands"));
 
 export let helpMessage = "";
 
-const commandsWithoutHelp = commands.filter((command) => command !== "help.ts");
+const commandsWithoutHelp = commands.filter(
+  (command) => !command.includes("help")
+);
+
+console.log(commands, commandsWithoutHelp);
 
 commandsWithoutHelp.forEach((filename) => {
   const commandFile = require(`../../commands/${filename}`);
