6dde19f078ff199104db3063c207a85846f69301
Paulo Abreu <paulo.santana@luby.software>
2022-09-25 00:58:15 -0300

    feat: ✨ add express on project

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
+import express, { Response } from "express";
 import { botStarted, sendMessage } from "./appServices/discord";
 import { discordClient } from "./shared/services/config/discord";
+import { envHelper } from "./shared/utils/envHelper";
+
+const PORT = envHelper.port;
+
+const app = express();
 
 discordClient.on("ready", botStarted);
 discordClient.on("messageCreate", sendMessage);
+
+app.use("/", (_, res: Response) => {
+  res.json({
+    message: "bot is running",
+    date: new Date().toISOString(),
+  });
+});
+
+app.listen(PORT || 3001);
